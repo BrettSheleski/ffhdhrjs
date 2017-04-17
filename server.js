ca0b069dfcfaecfb221e5eb3df432fc48f3ab994
Brett Sheleski <brett@sheleski.com>
2017-04-17 11:38:54 -0500

    refactoring and stuff

--- server.js
+++ server.js
@@ -1,64 +1,86 @@
-var serverPort = 2957;
-var hdHomeRun = {
-  'port' : 5004,
-  'hostname' : '192.168.0.131'
-};
-
-var transcodeFormats = {
-    'webm' : {
-        ffmpegOptions : {
-            "-c:v": "libvpx",
-            "-c:a" : "libvorbis",
-            "-f" : "webm",
-            "-crf" : "10"
-        },
-        contentType : "video/webm"
+var _config = {
+    serverPort: 2957,
+    hdHomeRune: {
+        'port': 5004,
+        'hostname': '127.0.0.1',
+        'defaultProfile': 'mobile'
+    },
+    transcoders: {
+        'webm': {
+            ffmpegOptions: {
+                "-c:v": "libvpx",
+                "-c:a": "libvorbis",
+                "-f": "webm",
+                "-crf": "10"
+            },
+            contentType: "video/webm"
+        }
     }
-}
+};
 
 var http = require('http'),
     express = require('express'),
     child_process = require("child_process"),
-    os = require('os');
+    os = require('os'),
+    fs = require('fs');
+
+var _defaultTranscoder = null;
+
+(function () {
+    const hdhrJson = __dirname + "/hdhr.json";
+    const transcodersJson = __dirname + "/transcoders.json";
+    if (fs.existsSync(hdhrJson)) {
+        _config.hdHomeRun = JSON.parse(fs.readFileSync(hdhrJson, "utf8"));
+    }
+
+    if (fs.existsSync(transcodersJson)) {
+        _config.transcoders = JSON.parse(fs.readFileSync(transcodersJson, "utf8"));
+    }
+
+    for (var key in _config.transcoders) {
+        _defaultTranscoder = _config.transcoders[key];
+        break;
+    }
+})();
 
 var app = express();
 
-app.get("/lineup.*", function(request, response, next){
+app.get("/lineup.*", function (request, response, next) {
     var options = {
         method: 'GET',
-        hostname: hdHomeRun.hostname, 
-        port: 80, 
+        hostname: _config.hdHomeRun.hostname,
+        port: 80,
         path: request.path
     };
 
-    var hdhrreq = http.request(options, function(hdhrres) {
+    var hdhrreq = http.request(options, function (hdhrres) {
         response.writeHead(hdhrres.statusCode, hdhrres.statusMessage, hdhrres.headers);
         hdhrres.pipe(response);
     })
-    .end();
+        .end();
 });
 
-app.get('/ch/:channel/:source?', function(request, response, next) {
+app.get('/ch/:channel/:source?', function (request, response, next) {
 
-   console.log("tuning to channel " + request.params.channel);
+    console.log("tuning to channel " + request.params.channel);
 
-   request.on('end', function(){
+    request.on('end', function () {
         console.log('END REQUEST');
-   });
+    });
 
     var options = {
         method: 'GET',
-        hostname: hdHomeRun.hostname, 
-        port: hdHomeRun.port, 
-        path: "/auto/v" + request.params.channel + "?transcode=" + (request.params.source || "heavy")
+        hostname: _config.hdHomeRun.hostname,
+        port: _config.hdHomeRun.port,
+        path: "/auto/v" + request.params.channel + "?transcode=" + (request.params.source || _config.hdHomeRun.defaultProfile)
     };
 
     var ffmpegCommand = null;
 
-    var hdhrreq = http.request(options, function(hdhrres) {
-        if (hdhrres.statusCode != 200){
+    var hdhrreq = http.request(options, function (hdhrres) {
+        if (hdhrres.statusCode !== 200) {
             console.log("Unable to tune to channel " + request.params.channel);
-            response.writeHead(hdhrres.statusCode, hdhrres.statusMessage, hdhrres.headers)
+            response.writeHead(hdhrres.statusCode, hdhrres.statusMessage, hdhrres.headers);
             response.end();
             return;
         }
@@ -69,37 +91,34 @@ app.get('/ch/:channel/:source?', function(request, response, next) {
 
         console.log("piping channel " + request.params.channel + " to ffmpeg");
 
-        
-
-        response.writeHead(hdhrres.statusCode, hdhrres.statusMessage, hdhrres.headers)
+        response.writeHead(hdhrres.statusCode, hdhrres.statusMessage, hdhrres.headers);
 
         var ffmpegOptions = "-re -i -";
-        var ffmpegTranscodeOptions = transcodeFormats['webm'];
-        
+        var ffmpegTranscodeOptions = _defaultTranscoder;
 
-        if ("format" in request.query && request.query["format"] in transcodeFormats){
-            ffmpegTranscodeOptions = transcodeFormats[request.query["format"]];
+        if ("format" in request.query && request.query["format"] in _config.transcoders) {
+            ffmpegTranscodeOptions = _config.transcoders[request.query["format"]];
         }
 
         hdhrres.headers["content-type"] = ffmpegTranscodeOptions.contentType;
 
-        for(var key in ffmpegTranscodeOptions.ffmpegOptions){
+        for (var key in ffmpegTranscodeOptions.ffmpegOptions) {
             ffmpegOptions += " " + key;
             ffmpegOptions += " " + ffmpegTranscodeOptions.ffmpegOptions[key];
         }
 
-        if (width){
-            if (!height){
+        if (width) {
+            if (!height) {
                 height = -1;
             }
 
             ffmpegOptions += " -vf scale=" + width.toString() + ":" + height.toString();
-        } 
-        else if (height){
+        }
+        else if (height) {
             ffmpegOptions += " -vf scale=-1:" + height.toString();
         }
 
-        if (bitrate){
+        if (bitrate) {
             ffmpegOptions += " -b:v 1M";
         }
 
@@ -108,33 +127,48 @@ app.get('/ch/:channel/:source?', function(request, response, next) {
         console.log("ffmpeg options:", ffmpegOptions);
 
         ffmpegCommand = child_process.spawn("ffmpeg", ffmpegOptions.split(' '));
-        ffmpegCommand.on('error', function(e){
+        ffmpegCommand.on('error', function (e) {
             console.log(e);
-            
+
             response.end();
         });
 
         ffmpegCommand.stdout.pipe(response);
-        
+
         hdhrres.pipe(ffmpegCommand.stdin);
     })
-    .on('end', function(){
-        console.log('request ended')
+        .on('end', function () {
+            console.log('request ended');
 
-        if (ffmpegCommand != null){
-            ffmpegCommand.kill();
-        }
-    })
-    .on('error', function(e){
-        console.log(e);
-        response.write("ERROR!!!");
-    })
-    .end();
+            if (ffmpegCommand !== null) {
+                ffmpegCommand.kill();
+            }
+        })
+        .on('error', function (e) {
+            console.log(e);
+            response.write("ERROR!!!");
+        })
+        .end();
 
-})
-.use(express.static('public'))
-.listen(serverPort, function(){
-	console.log("listening on port " + serverPort.toString());
-    
-	console.log("\thttp://192.168.0.2:" + serverPort.toString() + "/");
 });
+
+app.use(express.static('public'));
+
+app.listen(_config.serverPort, function () {
+    console.log("listening on port " + _config.serverPort.toString());
+
+        console.log("*******************************************")
+        console.log("*")
+        var interfaces = os.networkInterfaces();
+        var addresses = [];
+        for (var k in interfaces) {
+            for (var k2 in interfaces[k]) {
+                var address = interfaces[k][k2];
+                if (address.family === 'IPv4') {
+                    console.log("*\thttp://" + address.address + ":" + _config.serverPort.toString() + "/");
+                }
+            }
+        }
+        console.log("*")
+        console.log("*******************************************")
+    });
